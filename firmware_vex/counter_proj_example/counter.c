94f11639cc8a12857c6b566400f7e9b5158887cd
jeffdi <jeffdi@efabless.com>
2023-01-19 12:07:46 -0800

    add counter example firmware

--- firmware_vex/counter_proj_example/counter.c
+++ firmware_vex/counter_proj_example/counter.c
@@ -21,35 +21,35 @@ void main()
     gpio_config_io();
 
     // Left side I/O
-    reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_36 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_33 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_32 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_31 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_30 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_29 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_28 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_27 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_26 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_25 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_24 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_23 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_22 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_21 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_20 = GPIO_MODE_MGMT_STD_OUTPUT;
-    reg_mprj_io_19 = GPIO_MODE_MGMT_STD_OUTPUT;
+    reg_mprj_io_37 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_36 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_33 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_32 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_31 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_30 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_29 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_28 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_27 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_26 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_25 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_24 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_23 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_22 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_21 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_20 = GPIO_MODE_USER_STD_OUTPUT;
+    reg_mprj_io_19 = GPIO_MODE_USER_STD_OUTPUT;
 
     // Right side I/O
-	reg_mprj_io_18 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_17 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_16 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_15 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_14 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_13 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_12 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_11 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_10 =   GPIO_MODE_MGMT_STD_OUTPUT;
-	reg_mprj_io_9 =   GPIO_MODE_MGMT_STD_OUTPUT;
+	reg_mprj_io_18 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_17 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_16 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_15 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_14 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_13 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_12 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_11 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_10 =   GPIO_MODE_USER_STD_OUTPUT;
+	reg_mprj_io_9 =   GPIO_MODE_USER_STD_OUTPUT;
 
 	reg_mprj_io_8 =   GPIO_MODE_USER_STD_OUTPUT;
 	reg_mprj_io_7 =   GPIO_MODE_USER_STD_OUTPUT;
@@ -60,8 +60,6 @@ void main()
 	reg_mprj_io_2 =   GPIO_MODE_USER_STD_OUTPUT;
 	reg_mprj_io_1 =   GPIO_MODE_USER_STD_OUTPUT;
 	reg_mprj_io_0 =   GPIO_MODE_USER_STD_OUTPUT;
-//	reg_mprj_io_1 =   GPIO_MODE_MGMT_STD_OUTPUT;
-//	reg_mprj_io_0 =   GPIO_MODE_MGMT_STD_OUTPUT;
 
 	reg_gpio_out = 1; // OFF
     reg_gpio_mode1 = 1;
