86268625ce1dd2fc7038d6ccbbcd6af9db37c841
jeffdi <jeffdi@efabless.com>
2023-02-01 13:44:06 -0800

    update documentation and correct powerup for Nucleo boot

--- firmware_vex/nucleo/main.py
+++ firmware_vex/nucleo/main.py
@@ -3,3 +3,8 @@ import io_config
 # sw = pyb.Switch()
 # sw.callback(io_config.run())
 # io_config.run()
+from nucleo_api import *
+test = Test()
+test.voltage = 1.6
+test.powerup_sequence()
+test.release_reset()
